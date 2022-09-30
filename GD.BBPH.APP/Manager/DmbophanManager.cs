1c29bfddc74c024646a0f5e856819b7814b5b36a
DESKTOP-QD7JEVF\ADMIN <thepoorman1998@gmail.com>
2022-09-30 16:43:26 +0700

    Cường - Danh mục bộ phận

---
+++ GD.BBPH.APP/Manager/DmbophanManager.cs
@@ -0,0 +1,31 @@
+
+/*
+'===============================================================================
+'  GD.BBPH.BL.MenuManager
+'===============================================================================
+*/
+
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using GD.BBPH.BLL;
+using GD.BBPH.DAL;
+using GD.BBPH.DAL.EntityClasses;
+using GD.BBPH.DAL.FactoryClasses;
+using GD.BBPH.DAL.HelperClasses;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace GD.BBPH.BLL
+{
+	public class DmbophanManager : DmbophanManagerBase
+	{
+		/// <summary>
+		/// Purpose: Class constructor.
+		/// </summary>
+		public DmbophanManager()
+		{
+			// Nothing for now.
+		}
+	}
+}
