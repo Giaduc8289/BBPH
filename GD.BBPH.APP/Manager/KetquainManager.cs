1277b4488eef6399b9475898f9f941fe5a7fb5aa
DESKTOP-QD7JEVF\ADMIN <linhnguyenyoyo99@gmail.com>
2022-10-14 17:37:41 +0700

    Linh - chỉnh sửa thống kê kết quả in

--- GD.BBPH.APP/Manager/KetquainManager.cs
+++ GD.BBPH.APP/Manager/KetquainManager.cs
@@ -27,5 +27,23 @@ namespace GD.BBPH.BLL
 		{
 			// Nothing for now.
 		}
-	}
+        public DataTable SelectByNgayCaMamayRDT(System.DateTime Ngay, System.Int32 Ca, System.String Mamay)
+        {
+            DataTable toReturn = new DataTable();
+            EntityCollection _KetquainCollection = new EntityCollection(new KetquainEntityFactory());
+            RelationPredicateBucket filter = new RelationPredicateBucket();
+
+            IPredicateExpression _PredicateExpression = new PredicateExpression();
+            _PredicateExpression.Add(KetquainFields.Ngay == Ngay);
+            _PredicateExpression.Add(KetquainFields.Ca == Ca);
+            _PredicateExpression.Add(KetquainFields.Mamay == Mamay);
+            filter.PredicateExpression.Add(_PredicateExpression);
+
+            using (DataAccessAdapterBase adapter = (new DataAccessAdapterFactory()).CreateAdapter())
+            {
+                adapter.FetchTypedList(_KetquainCollection.EntityFactoryToUse.CreateFields(), toReturn, filter, true);
+            }
+            return toReturn;
+        }
+    }
 }
