b88ca358fddeca9b543cfdd3139e128b300059ab
DESKTOP-V5VV5LL\Adminstrators <hungtv2012@gmail.com>
2022-10-25 17:20:31 +0700

    Tính công suất, Tính năng suất

---
+++ Tailieu/CSDL/Procedure/Tinhcongsuatmay.sql
@@ -0,0 +1,57 @@
+﻿------------------------Hàm tính công suất-------------
+If Object_ID('dbo.fTinhcongsuatmay','FN') is not null
+	Drop FUNCTION dbo.fTinhcongsuatmay
+Go
+
+Create FUNCTION dbo.fTinhcongsuatmay(@Mamay Nvarchar(50), @Masanpham Nvarchar(50)) RETURNS DECIMAL(20,2)
+AS
+BEGIN
+
+	Declare @v_Madongmay	Nvarchar(50),
+			@v_Doday		Decimal(20,3),
+			@v_Rong				Decimal(20,3),
+			@v_Dai				Decimal(20,3),
+			@v_Somauma				Nvarchar(50),
+			@v_Sohinhma				Nvarchar(50),
+			@v_Maloaimang			Nvarchar(50),
+			@v_Maqcthanhpham		Nvarchar(50),
+			@v_Tocdo			Decimal(20,3)
+
+	Select @v_Madongmay=Madm From dmmay Where Mamay=@Mamay
+	
+	If (@v_Madongmay='TH')
+	Begin
+		Select @v_Doday=Doday From Dmmang Where Mamang=@Masanpham	
+
+		Select @v_Tocdo=Tocdo
+		From congsuatmay
+		Where (Mamay=@Mamay Or Mamay='')
+			And ((Dodaytu<=@v_Doday And Dodayden>=@v_Doday) Or (Dodaytu=0 And Dodayden=0))	
+	End
+	Else
+	Begin
+		Select @v_Doday=Doday, @v_Rong=Rong, @v_Dai=Dai
+			, @v_Somauma=Somauma, @v_Sohinhma=Sohinhma, @v_Maqcthanhpham=Maqcthanhpham
+		From dmhang Where Masp=@Masanpham
+		
+		Select @v_Tocdo=Tocdo
+		From congsuatmay
+		Where (Mamay=@Mamay Or Mamay='')
+			And (Madongmay=@v_Madongmay Or Madongmay='')
+			And ((Dodaytu<=@v_Doday And Dodayden>=@v_Doday) Or (Dodaytu=0 And Dodayden=0))	
+			And ((Khotu<=@v_Rong And Khoden>=@v_Rong) Or (Khotu=0 And Khoden=0))	
+			And ((Chieudaitu<=@v_Dai And Chieudaiden>=@v_Dai) Or (Chieudaitu=0 And Chieudaiden=0))	
+			AND	(CHARINDEX(@v_Sohinhma+',',Sohinhma)>0 OR ISNULL(Sohinhma,'') = '')
+			AND	(CHARINDEX(@v_Somauma+',',Somauma)>0 OR ISNULL(Somauma,'') = '')
+			AND (CHARINDEX(@v_Maqcthanhpham+',',Maqcthanhpham)>0 OR ISNULL(Maqcthanhpham,'') = '')			
+	End
+
+	RETURN ISNULL(@v_Tocdo,0);
+END
+	
+Go
+
+SELECT dbo.fTinhcongsuatmay('T1', 'PE20x850')
+
+GO	
+
--- Tailieu/CSDL/Procedure/Tinhnangsuatthoi.sql
+++ Tailieu/CSDL/Procedure/Tinhnangsuatthoi.sql
@@ -8,41 +8,30 @@ AS
 BEGIN
 
 	Declare @v_Mamay		Nvarchar(50),
-			@v_Madongmay	Nvarchar(50),
+			--@v_Madongmay	Nvarchar(50),
 			@v_Mamang		Nvarchar(50),
-			@v_Doday		Decimal(20,3),
-			@v_Rong				Decimal(20,3),
+			--@v_Doday		Decimal(20,3),
+			--@v_Rong		Decimal(20,3),
+			--@v_Dai		Decimal(20,3),
 			@v_Sokgdukien		Decimal(20,3),
 			@v_Sokgthucte		Decimal(20,3),
-			@v_Somet			Decimal(20,3),
+			--@v_Somet			Decimal(20,3),
 			@v_Thoigianchay		Decimal(20,3),
 			@v_Tocdo			Decimal(20,3),
 			@v_Nangsuat			DECIMAL(20,3)
 
-	Select @v_Mamay=Mamay, @v_Mamang=Mamang, @v_Sokgthucte=Sokg, @v_Somet=Somet 
+	Select @v_Mamay=Mamay, @v_Mamang=Mamang, @v_Sokgthucte=Sokg--, @v_Somet=Somet 
 		, @v_Thoigianchay=DATEDIFF(Minute,Thoigianbatdau,Thoigianketthuc)-Thoigianchuanbi-Thoigiansuco
 	From Ketquathoi 
 	Where Id=@Ketquathoi_Id
---Select * from Ketquathoi
-	--Select @v_Madongmay=Madm From dmmay Where Mamay=@v_Mamay
 	
-	--Truy vấn thông tin sản phẩm
-	Select @v_Doday=Doday, @v_Rong=Rong From Dmmang Where Mamang=@v_Mamang	
---Select * from dmmang
-
 	--Tính chất lượng căn cứ: Độ dày * Rộng * Số mét * 0,925/1000000
-	
 	--Tính năng suất căn cứ: Thời gian chạy * Tốc độ
-	--Độ dày màng và Mã máy tính ra Công suất máy
-	Select @v_Tocdo=Tocdo
-	From congsuatmay
-	Where (Mamay=@v_Mamay Or Mamay='')
-		--And (Madongmay=@v_Madongmay Or Madongmay='')
-		And ((@v_Doday>=Dodaytu And @v_Doday<Dodayden) Or (Dodaytu=0 And Dodayden=0))	
---Select * from congsuatmay
+	Select @v_Tocdo=dbo.fTinhcongsuatmay(@v_Mamay, @v_Mamang)
 
 	Set @v_Sokgdukien=@v_Thoigianchay*@v_Tocdo
-	Set @v_Nangsuat=@v_Sokgthucte/@v_Sokgdukien
+	If (@v_Sokgdukien <> 0) 
+		Set @v_Nangsuat=@v_Sokgthucte/@v_Sokgdukien
 		
 	RETURN ISNULL(@v_Nangsuat,0);
 END
