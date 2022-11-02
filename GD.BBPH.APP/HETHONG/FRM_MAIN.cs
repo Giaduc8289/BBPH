bd1ce11ecfbf66abbb65f9f9eb728154fedf6efd
DESKTOP-V5VV5LL\Adminstrators <hungtv2012@gmail.com>
2022-11-02 11:32:18 +0700

    Báo cáo tổng hợp kết quả in

--- GD.BBPH.APP/HETHONG/FRM_MAIN.cs
+++ GD.BBPH.APP/HETHONG/FRM_MAIN.cs
@@ -1029,6 +1029,13 @@ namespace GD.BBPH.APP.HETHONG
                     }
                     ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BCKQINCHITIET()), mmru.Text);
                     break;
+                case "FRM_BCKQINTONGHOP":
+                    foreach (GD.BBPH.CONTROL.FORM frm in this.MdiChildren)
+                    {
+                        if (frm.Name == "FRM_BCKQINTONGHOP") { frm.Activate(); return; }
+                    }
+                    ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BCKQINTONGHOP()), mmru.Text);
+                    break;
                 #endregion
 
                 #region Cửa sổ
---
+++ Tailieu/CSDL/Procedure/Baocaoketquaintonghop.sql
@@ -0,0 +1,31 @@
+﻿------------------------Kết quả in tổng hợp-------------
+If Object_ID('dbo.Baocaoketquaintonghop','P') is not null
+	Drop Procedure dbo.Baocaoketquaintonghop;
+Go
+Create Procedure dbo.Baocaoketquaintonghop
+	@Tungay			Date,
+	@Denngay		DATE,
+	@Mamay			NVARCHAR(50),
+	@Makhach		NVARCHAR(50),
+	@Masanpham		Nvarchar(50)
+With Encryption As
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+	
+	SELECT Makhach, Tenkhach, Masanpham, Tensanpham, Ngay, Sum(SometLSX) As SoluongLSX 
+		, Sum(Sometcap) As Dauvaomet, Sum(Sokgcap) As Dauvaokg
+		, Sum(Thuctein) As Dauramet, Sum(Sokgin) As Daurakg
+		, Avg(Nangsuat) As Nangsuat, Avg(Tylesaihong) As Tylesaihong
+	FROM dbo.Ketquain kq Left Join dmhang sp on kq.Masanpham=sp.Masp
+	WHERE Ngay BETWEEN @Tungay AND @Denngay 
+		AND (Mamay = @Mamay OR @Mamay='')
+		AND (Makhach = @Makhach OR @Makhach = '')
+		AND (kq.Masanpham = @Masanpham OR @Masanpham = '')
+	Group By Makhach, Tenkhach, Masanpham, Tensanpham, Ngay
+	ORDER BY Makhach, Tenkhach, Masanpham, Tensanpham, Ngay
+	
+GO
+
+EXEC Baocaoketquaintonghop '10/01/2022', '11/30/2022','','',''
+
+
