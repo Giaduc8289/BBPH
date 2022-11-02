7ba2a0dc25aa39e7919f385179b2e63c60d6459e
DESKTOP-QD7JEVF\ADMIN <thepoorman1998@gmail.com>
2022-11-02 15:19:20 +0700

    cường - báo cáo kết quả tua chi tiết

--- GD.BBPH.APP/HETHONG/FRM_MAIN.cs
+++ GD.BBPH.APP/HETHONG/FRM_MAIN.cs
@@ -1036,6 +1036,14 @@ namespace GD.BBPH.APP.HETHONG
                     }
                     ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BCKQINTONGHOP()), mmru.Text);
                     break;
+
+                case "FRM_BCKQTUACHITIET":
+                    foreach (GD.BBPH.CONTROL.FORM frm in this.MdiChildren)
+                    {
+                        if (frm.Name == "FRM_BCKQTUACHITIET") { frm.Activate(); return; }
+                    }
+                    ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BCKQTUACHITIET()), mmru.Text);
+                    break;
                 #endregion
 
                 #region Cửa sổ
---
+++ Tailieu/CSDL/Procedure/Baocaoketquatuachitiet.sql
@@ -0,0 +1,31 @@
+﻿------------------------Kết quả in chi tiết-------------
+If Object_ID('dbo.Baocaoketquatuachitiet','P') is not null
+	Drop Procedure dbo.Baocaoketquatuachitiet;
+Go
+Create Procedure dbo.Baocaoketquatuachitiet
+	@Tungay			Date,
+	@Denngay		DATE,
+	@Mamay			NVARCHAR(50),
+	@Makhach		NVARCHAR(50),
+	@Masanpham		Nvarchar(50)
+With Encryption As
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+	
+	SELECT Ngay, Ca, Mamay, Tenmay, Lenhtua As Lenhsanxuat
+		, Masanpham, Tensanpham, Makhach, Tenkhach, Macongnhan, Tencongnhan
+		, SoMetvao As Dauvaomet, SoKgvao As Dauvaokg
+		, SoMetra As Dauramet, SoKgra As Daurakg
+		, Nangsuat, Chenhlechkg
+	FROM dbo.Ketquatua kq Left Join dmhang sp on kq.Masanpham=sp.Masp
+	WHERE Ngay BETWEEN @Tungay AND @Denngay 
+		AND (Mamay = @Mamay OR @Mamay='')
+		AND (Makhach = @Makhach OR @Makhach = '')
+		AND (kq.Masanpham = @Masanpham OR @Masanpham = '')
+	ORDER BY Ngay, Mamay, Ca
+	
+GO
+
+EXEC Baocaoketquatuachitiet '10/01/2022', '11/30/2022','','',''
+
+
