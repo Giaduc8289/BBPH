fe41471780f008513697eef94dfbbf5c58cc3880
DESKTOP-QD7JEVF\ADMIN <thepoorman1998@gmail.com>
2022-11-03 14:52:52 +0700

    app, frm_main

--- GD.BBPH.APP/HETHONG/FRM_MAIN.cs
+++ GD.BBPH.APP/HETHONG/FRM_MAIN.cs
@@ -1059,6 +1059,13 @@ namespace GD.BBPH.APP.HETHONG
                     }
                     ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BCKQCHIACHITIET()), mmru.Text);
                     break;
+                case "FRM_BCKQHOANTHIENCHITIET":
+                    foreach (GD.BBPH.CONTROL.FORM frm in this.MdiChildren)
+                    {
+                        if (frm.Name == "FRM_BCKQHOANTHIENCHITIET") { frm.Activate(); return; }
+                    }
+                    ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BCKQHOANTHIENCHITIET()), mmru.Text);
+                    break;
                 #endregion
 
                 #region Cửa sổ
