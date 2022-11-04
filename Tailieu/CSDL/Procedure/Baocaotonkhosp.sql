4d70ab4a3b3a04be78dff02e3a737473b2af0c6e
DESKTOP-QD7JEVF\ADMIN <linhnguyenyoyo99@gmail.com>
2022-11-04 16:05:28 +0700

    Linh - báo cáo tồn kho sản phẩm

---
+++ Tailieu/CSDL/Procedure/Baocaotonkhosp.sql
@@ -0,0 +1,69 @@
+﻿------------------------Báo cáo tồn kho sản phẩm-------------
+If Object_ID('dbo.Baocaotonkhosp','P') is not null
+	Drop Procedure dbo.Baocaotonkhosp;
+Go
+Create Procedure dbo.Baocaotonkhosp
+	@Ngay			Date,
+	@Makho			NVARCHAR(50),
+	@Masanpham		Nvarchar(50)
+With Encryption As
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+	
+	Declare @v_Ngaykiemke		Date
+	
+	--Lấy ngày kiểm kê gần nhất
+	Select @v_Ngaykiemke=dbo.fNgaykiemkesanpham(@Ngay)
+	
+	--Lấy số liệu tại thời điểm kiểm kê gần nhất
+	Select Makho, Tenkho, Masanpham, Tensanpham
+		, SUM(isNull(Somet,0)) As Somet, SUM(isNull(Sokg,0)) As Sokg, SUM(isNull(Soluong,0)) As Soluong
+	Into #Sodu
+	From Sodusanpham
+	Where Ngaykiemke=@v_Ngaykiemke
+		And (Makho=@Makho Or @Makho='')
+		And (Masanpham=@Masanpham Or @Masanpham='')
+	Group By Makho, Tenkho, Masanpham, Tensanpham
+	
+	--Lấy số liệu nhập kho từ ngày kiểm kê đến ngày báo cáo
+	Select Makho, Tenkho, Masanpham, Tensanpham
+		, SUM(isNull(Somet,0)) As Somet, SUM(isNull(Sokg,0)) As Sokg, SUM(isNull(Soluong,0)) As Soluong
+	Into #Nhap
+	From Nhapkhosanpham
+	Where Ngaynhap BETWEEN @v_Ngaykiemke And @Ngay
+		And (Makho=@Makho Or @Makho='')
+		And (Masanpham=@Masanpham Or @Masanpham='')
+	Group By Makho, Tenkho, Masanpham, Tensanpham
+	
+	--Lấy số liệu xuất kho từ ngày kiểm kê đến ngày báo cáo
+	Select Makho, Tenkho, Masanpham, Tensanpham
+		, SUM(isNull(Somet,0)) As Somet, SUM(isNull(Sokg,0)) As Sokg, SUM(isNull(Soluong,0)) As Soluong
+	Into #Xuat
+	From Xuatkhosanpham
+	Where Ngayxuat BETWEEN @v_Ngaykiemke And @Ngay
+		And (Makho=@Makho Or @Makho='')
+		And (Masanpham=@Masanpham Or @Masanpham='')
+	Group By Makho, Tenkho, Masanpham, Tensanpham
+	
+	Select Makho, Tenkho, Masanpham, Tensanpham
+		, Somet, Sokg, Soluong
+	Into #Ketqua
+	From #Sodu
+	UNION
+	Select Makho, Tenkho, Masanpham, Tensanpham
+		, Somet, Sokg, Soluong
+	From #Nhap
+	UNION
+	Select Makho, Tenkho, Masanpham, Tensanpham
+		, -Somet, -Sokg, -Soluong
+	From #Xuat
+	
+	Select Makho, Tenkho, Masanpham, Tensanpham
+		, SUM(Somet) As Somet, SUM(Sokg) As Sokg, SUM(Soluong) As Soluong
+	From #Ketqua
+	Group By Makho, Tenkho, Masanpham, Tensanpham
+	
+GO
+
+EXEC Baocaotonkhosp '11/30/2022','',''
+
---
+++ Tailieu/CSDL/Procedure/Tinhngaykiemke.sql
@@ -0,0 +1,80 @@
+﻿------------------------Ngày kiểm kê sản phẩm-------------
+If Object_ID('dbo.fNgaykiemkesanpham','FN') is not null
+	Drop FUNCTION dbo.fNgaykiemkesanpham
+Go
+
+Create FUNCTION dbo.fNgaykiemkesanpham(@Ngay DATE) RETURNS DATE
+AS
+BEGIN
+	DECLARE @v_Ngaykiemke		DATE
+	
+	SELECT @v_Ngaykiemke=MAX(Ngaykiemke)
+	FROM dbo.Sodusanpham
+	WHERE Ngaykiemke <= @Ngay 
+	
+	IF (ISNULL(@v_Ngaykiemke,'')='')
+		SET @v_Ngaykiemke = CONVERT(DATE,'01/01/' + CONVERT(NVARCHAR(4),YEAR(@Ngay)), 103)
+	
+	RETURN @v_Ngaykiemke
+END
+	
+Go
+
+SELECT dbo.fNgaykiemkesanpham('01/06/2022')
+
+GO	
+
+------------------------Ngày kiểm kê màng-------------
+If Object_ID('dbo.fNgaykiemkemang','FN') is not null
+	Drop FUNCTION dbo.fNgaykiemkemang
+Go
+
+Create FUNCTION dbo.fNgaykiemkemang(@Ngay DATE) RETURNS DATE
+AS
+BEGIN
+	DECLARE @v_Ngaykiemke		DATE
+	
+	SELECT @v_Ngaykiemke=MAX(Ngaykiemke)
+	FROM dbo.Sodumang
+	WHERE Ngaykiemke <= @Ngay 
+	
+	IF (ISNULL(@v_Ngaykiemke,'')='')
+		SET @v_Ngaykiemke = CONVERT(DATE,'01/01/' + CONVERT(NVARCHAR(4),YEAR(@Ngay)), 103)
+	
+	RETURN @v_Ngaykiemke
+END
+	
+Go
+
+SELECT dbo.fNgaykiemkemang('01/06/2022')
+
+GO	
+
+
+
+------------------------Ngày kiểm kê nguyên liệu-------------
+If Object_ID('dbo.fNgaykiemkenguyenlieu','FN') is not null
+	Drop FUNCTION dbo.fNgaykiemkenguyenlieu
+Go
+
+Create FUNCTION dbo.fNgaykiemkenguyenlieu(@Ngay DATE) RETURNS DATE
+AS
+BEGIN
+	DECLARE @v_Ngaykiemke		DATE
+	
+	SELECT @v_Ngaykiemke=MAX(Ngaykiemke)
+	FROM dbo.Sodunguyenlieu
+	WHERE Ngaykiemke <= @Ngay 
+	
+	IF (ISNULL(@v_Ngaykiemke,'')='')
+		SET @v_Ngaykiemke = CONVERT(DATE,'01/01/' + CONVERT(NVARCHAR(4),YEAR(@Ngay)), 103)
+	
+	RETURN @v_Ngaykiemke
+END
+	
+Go
+
+SELECT dbo.fNgaykiemkenguyenlieu('01/06/2022')
+
+GO	
+
