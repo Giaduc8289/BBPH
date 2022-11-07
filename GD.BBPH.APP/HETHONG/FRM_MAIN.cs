4d7d1cc7037b50887b53895d17b5d022b59b7e93
DESKTOP-QD7JEVF\ADMIN <linhnguyenyoyo99@gmail.com>
2022-11-07 11:50:24 +0700

    Linh - Báo cáo xuất kho nguyên liệu

--- GD.BBPH.APP/HETHONG/FRM_MAIN.cs
+++ GD.BBPH.APP/HETHONG/FRM_MAIN.cs
@@ -1143,6 +1143,13 @@ namespace GD.BBPH.APP.HETHONG
                     }
                     ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BAOCAONHAPXUATTONKHOSP()), mmru.Text);
                     break;
+                case "FRM_BAOCAOXUATNGUYENLIEU":
+                    foreach (GD.BBPH.CONTROL.FORM frm in this.MdiChildren)
+                    {
+                        if (frm.Name == "FRM_BAOCAOXUATNGUYENLIEU") { frm.Activate(); return; }
+                    }
+                    ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BAOCAOXUATNGUYENLIEU()), mmru.Text);
+                    break;
                 #endregion
 
                 #region Cửa sổ
