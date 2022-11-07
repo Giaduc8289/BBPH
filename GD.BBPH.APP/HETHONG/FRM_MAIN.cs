000076aec66ace4e15614689d6cf76960e0f8a89
DESKTOP-QD7JEVF\ADMIN <linhnguyenyoyo99@gmail.com>
2022-11-07 14:27:19 +0700

    Linh - báo cáo nhập kho nguyên liệu

--- GD.BBPH.APP/HETHONG/FRM_MAIN.cs
+++ GD.BBPH.APP/HETHONG/FRM_MAIN.cs
@@ -1150,6 +1150,13 @@ namespace GD.BBPH.APP.HETHONG
                     }
                     ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BAOCAOXUATNGUYENLIEU()), mmru.Text);
                     break;
+                case "FRM_BAOCAONHAPNGUYENLIEU":
+                    foreach (GD.BBPH.CONTROL.FORM frm in this.MdiChildren)
+                    {
+                        if (frm.Name == "FRM_BAOCAONHAPNGUYENLIEU") { frm.Activate(); return; }
+                    }
+                    ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BAOCAONHAPNGUYENLIEU()), mmru.Text);
+                    break;
                 #endregion
 
                 #region Cửa sổ
---
+++ Tailieu/CSDL/Procedure/Baocaonhapnguyenlieu.sql
@@ -0,0 +1,26 @@
+﻿------------------------Báo cáo nhập kho nguyên liệu-------------
+If Object_ID('dbo.Baocaonhapnguyenlieu','P') is not null
+	Drop Procedure dbo.Baocaonhapnguyenlieu;
+Go
+Create Procedure dbo.Baocaonhapnguyenlieu
+	@Tungay			Date,
+	@Denngay		DATE,
+	@Makho			NVARCHAR(50),
+	@Manguyenlieu		Nvarchar(50),
+	@Manhomnguyenlieu		Nvarchar(50)
+With Encryption As
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+	
+	SELECT Ngaynhap As Ngay, Makho, Tenkho, Manguyenlieu, Tennguyenlieu,
+		   Soluong, Donvitinh, Malydo, Tenlydo, Sohopdongmua
+	FROM dbo.Nhapkhonguyenlieu xk Left Join dbo.dmnguyenlieu nl on xk.Manguyenlieu = nl.Manl
+	WHERE Ngaynhap BETWEEN @Tungay AND @Denngay 
+		AND (Makho = @Makho OR @Makho='')
+		AND (Manguyenlieu = @Manguyenlieu OR @Manguyenlieu = '')
+		AND (Manhom = @Manhomnguyenlieu OR @Manhomnguyenlieu = '')
+	ORDER BY Ngay, Tenkho,Tennhom, Tennguyenlieu
+	
+GO
+
+EXEC Baocaonhapnguyenlieu '10/01/2022', '11/30/2022','','',''
