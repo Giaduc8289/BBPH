08a012b1c151c68c61c45f840ae7838c1f2ccfa6
Thien <kingoflegend102@gmail.com>
2022-11-17 10:34:20 +0700

    tong hop luong chia

--- GD.BBPH.APP/HETHONG/FRM_MAIN.cs
+++ GD.BBPH.APP/HETHONG/FRM_MAIN.cs
@@ -1264,6 +1264,13 @@ namespace GD.BBPH.APP.HETHONG
                     }
                     ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BCLUONGTOCHIA()), mmru.Text);
                     break;
+                case "FRM_BCTONGHOPLUONGTOCHIA":
+                    foreach (GD.BBPH.CONTROL.FORM frm in this.MdiChildren)
+                    {
+                        if (frm.Name == "FRM_BCTONGHOPLUONGTOCHIA") { frm.Activate(); return; }
+                    }
+                    ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BCTONGHOPLUONGTOCHIA()), mmru.Text);
+                    break;
                 #endregion
 
                 #endregion
