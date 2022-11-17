8f8e70fd377d06801f06d608ea09f07886a0b016
DESKTOP-QD7JEVF\ADMIN <thepoorman1998@gmail.com>
2022-11-17 09:39:41 +0700

    cường - báo cáo tổng hợp lương tổ thổi + tổ ghép

---
+++ Tailieu/CSDL/Procedure/Baocaotonghopluongtoghep.sql
@@ -0,0 +1,50 @@
+﻿------------------------Báo cáo tổng hợp lương tổ in-------------
+If Object_ID('dbo.Baocaotonghopluongtoghep','P') is not null
+	Drop Procedure dbo.Baocaotonghopluongtoghep
+Go
+
+Create Procedure Baocaotonghopluongtoghep
+	@Tungay			Date,
+	@Denngay		Date,
+	@Macongnhan		Nvarchar(50)
+--WITH Encryption As
+As	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+	
+	SELECT Ngay, Macongnhan, Tencongnhan
+		, Masanpham, Tensanpham
+		, SUM(ISNULL(MangcapMet,0)) Sometcap, SUM(ISNULL(MangcapKg,0)) Sokgcap
+		, SUM(ISNULL(ThucteMet,0)) Somet, SUM(ISNULL(ThucteKg,0)) Sokg
+	INTO #Ketquaghep0 
+	FROM dbo.Ketquaghep
+	WHERE Ngay BETWEEN @Tungay AND @Denngay
+	GROUP BY Ngay, Macongnhan,Tencongnhan, Masanpham, Tensanpham
+
+	Select Ngay, Masanpham, Tensanpham
+			--, Sometin, Sokgin
+			, Sometcap, Sokgcap, Somet, Sokg
+			, (Select COUNT(*) From dbo.SplitString(kq.Macongnhan,',')) As Socongnhan
+			, cn.Value As Macongnhan
+	Into #Ketquaghep1
+	From #Ketquaghep0 kq 
+		Cross Apply (Select * From dbo.SplitString(kq.Macongnhan,',')) As cn
+
+	Select Ngay, Macongnhan, Tencongnhan, Masanpham, Tensanpham
+			, SUM(ISNULL(Sometcap,0)) Sometcap, SUM(ISNULL(Sokgcap,0)) Sokgcap
+			, SUM(ISNULL(Somet,0)) Somet, SUM(ISNULL(Sokg,0)) Sokg
+			, Socongnhan
+	Into #Ketquaghep
+	From #Ketquaghep1 kq Inner Join dmcongnhan dm On dm.Macn=kq.Macongnhan
+	Where (Macongnhan=@Macongnhan Or @Macongnhan='')
+	GROUP BY Ngay, Macongnhan, Tencongnhan, Masanpham, Tensanpham, Socongnhan
+	
+	Select Macongnhan, Tencongnhan
+			, SUM(Sometcap/Socongnhan)  Dauvaomet, SUM(Sokgcap/Socongnhan) Dauvaokg
+			, SUM(Somet/Socongnhan ) Dauramet, SUM(Sokg/Socongnhan) Daurakg
+	From #Ketquaghep
+	GROUP BY Macongnhan, Tencongnhan, Socongnhan
+	Order By Macongnhan 
+	
+GO
+
+EXEC Baocaotonghopluongtoghep '08/01/2022', '11/30/2022', ''
