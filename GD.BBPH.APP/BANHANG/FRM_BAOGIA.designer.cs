d7c1738b3f733f8d8bbbf5be00fe445d55d77578
DESKTOP-V5VV5LL\Adminstrators <hungtv2012@gmail.com>
2022-11-17 09:16:13 +0700

    In báo giá

--- GD.BBPH.APP/BANHANG/FRM_BAOGIA.designer.cs
+++ GD.BBPH.APP/BANHANG/FRM_BAOGIA.designer.cs
@@ -88,7 +88,7 @@
             // 
             this.uiPanelManager1.BeginPanelInfo();
             this.uiPanelManager1.AddDockPanelInfo(new System.Guid("07a915f4-063b-4962-a538-f0e669e38ca3"), Janus.Windows.UI.Dock.PanelDockStyle.Left, new System.Drawing.Size(494, 542), true);
-            this.uiPanelManager1.AddDockPanelInfo(new System.Guid("43b1eac6-97d8-4025-b6ba-6cafc1f384ad"), Janus.Windows.UI.Dock.PanelDockStyle.Fill, new System.Drawing.Size(895, 542), true);
+            this.uiPanelManager1.AddDockPanelInfo(new System.Guid("43b1eac6-97d8-4025-b6ba-6cafc1f384ad"), Janus.Windows.UI.Dock.PanelDockStyle.Fill, new System.Drawing.Size(932, 542), true);
             this.uiPanelManager1.AddFloatingPanelInfo(new System.Guid("07a915f4-063b-4962-a538-f0e669e38ca3"), new System.Drawing.Point(-1, -1), new System.Drawing.Size(-1, -1), false);
             this.uiPanelManager1.AddFloatingPanelInfo(new System.Guid("43b1eac6-97d8-4025-b6ba-6cafc1f384ad"), new System.Drawing.Point(-1, -1), new System.Drawing.Size(-1, -1), false);
             this.uiPanelManager1.EndPanelInfo();
@@ -118,7 +118,7 @@
             this.uiPanel1.InnerContainer = this.uiPanel1Container;
             this.uiPanel1.Location = new System.Drawing.Point(497, 3);
             this.uiPanel1.Name = "uiPanel1";
-            this.uiPanel1.Size = new System.Drawing.Size(895, 542);
+            this.uiPanel1.Size = new System.Drawing.Size(932, 542);
             this.uiPanel1.TabIndex = 4;
             this.uiPanel1.Text = "Chi tiết";
             // 
@@ -160,7 +160,7 @@
             this.uiPanel1Container.Controls.Add(this.label22);
             this.uiPanel1Container.Location = new System.Drawing.Point(1, 23);
             this.uiPanel1Container.Name = "uiPanel1Container";
-            this.uiPanel1Container.Size = new System.Drawing.Size(893, 518);
+            this.uiPanel1Container.Size = new System.Drawing.Size(930, 518);
             this.uiPanel1Container.TabIndex = 0;
             // 
             // txt_LYDO
@@ -171,16 +171,18 @@
             this.txt_LYDO.FormNametoShow = "";
             this.txt_LYDO.Is_DateTime = false;
             this.txt_LYDO.IsShowForm = false;
-            this.txt_LYDO.Location = new System.Drawing.Point(212, 170);
+            this.txt_LYDO.Location = new System.Drawing.Point(554, 147);
+            this.txt_LYDO.Multiline = true;
             this.txt_LYDO.Name = "txt_LYDO";
             this.txt_LYDO.Regular_Expression = null;
-            this.txt_LYDO.Size = new System.Drawing.Size(657, 20);
+            this.txt_LYDO.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_LYDO.Size = new System.Drawing.Size(364, 44);
             this.txt_LYDO.TabIndex = 10;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(172, 173);
+            this.label6.Location = new System.Drawing.Point(514, 148);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(33, 13);
             this.label6.TabIndex = 183;
@@ -189,7 +191,7 @@
             // chk_XACNHANBAOGIA
             // 
             this.chk_XACNHANBAOGIA.AutoSize = true;
-            this.chk_XACNHANBAOGIA.Location = new System.Drawing.Point(12, 173);
+            this.chk_XACNHANBAOGIA.Location = new System.Drawing.Point(362, 146);
             this.chk_XACNHANBAOGIA.Margin = new System.Windows.Forms.Padding(2);
             this.chk_XACNHANBAOGIA.Name = "chk_XACNHANBAOGIA";
             this.chk_XACNHANBAOGIA.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
@@ -206,16 +208,18 @@
             this.txt_THOIGIANGIAO.FormNametoShow = "";
             this.txt_THOIGIANGIAO.Is_DateTime = false;
             this.txt_THOIGIANGIAO.IsShowForm = false;
-            this.txt_THOIGIANGIAO.Location = new System.Drawing.Point(100, 112);
+            this.txt_THOIGIANGIAO.Location = new System.Drawing.Point(554, 92);
+            this.txt_THOIGIANGIAO.Multiline = true;
             this.txt_THOIGIANGIAO.Name = "txt_THOIGIANGIAO";
             this.txt_THOIGIANGIAO.Regular_Expression = null;
-            this.txt_THOIGIANGIAO.Size = new System.Drawing.Size(390, 20);
-            this.txt_THOIGIANGIAO.TabIndex = 6;
+            this.txt_THOIGIANGIAO.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_THOIGIANGIAO.Size = new System.Drawing.Size(364, 44);
+            this.txt_THOIGIANGIAO.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 115);
+            this.label4.Location = new System.Drawing.Point(479, 95);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(72, 13);
             this.label4.TabIndex = 180;
@@ -298,18 +302,18 @@
             this.txt_NOIDUNG.FormNametoShow = "";
             this.txt_NOIDUNG.Is_DateTime = false;
             this.txt_NOIDUNG.IsShowForm = false;
-            this.txt_NOIDUNG.Location = new System.Drawing.Point(554, 13);
+            this.txt_NOIDUNG.Location = new System.Drawing.Point(554, 40);
             this.txt_NOIDUNG.Multiline = true;
             this.txt_NOIDUNG.Name = "txt_NOIDUNG";
             this.txt_NOIDUNG.Regular_Expression = null;
             this.txt_NOIDUNG.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txt_NOIDUNG.Size = new System.Drawing.Size(315, 116);
-            this.txt_NOIDUNG.TabIndex = 8;
+            this.txt_NOIDUNG.Size = new System.Drawing.Size(364, 44);
+            this.txt_NOIDUNG.TabIndex = 5;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(500, 15);
+            this.label8.Location = new System.Drawing.Point(501, 40);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(50, 13);
             this.label8.TabIndex = 178;
@@ -350,16 +354,18 @@
             this.txt_PHUONGTHUCGIAO.FormNametoShow = "";
             this.txt_PHUONGTHUCGIAO.Is_DateTime = false;
             this.txt_PHUONGTHUCGIAO.IsShowForm = false;
-            this.txt_PHUONGTHUCGIAO.Location = new System.Drawing.Point(100, 86);
+            this.txt_PHUONGTHUCGIAO.Location = new System.Drawing.Point(100, 92);
+            this.txt_PHUONGTHUCGIAO.Multiline = true;
             this.txt_PHUONGTHUCGIAO.Name = "txt_PHUONGTHUCGIAO";
             this.txt_PHUONGTHUCGIAO.Regular_Expression = null;
-            this.txt_PHUONGTHUCGIAO.Size = new System.Drawing.Size(390, 20);
-            this.txt_PHUONGTHUCGIAO.TabIndex = 5;
+            this.txt_PHUONGTHUCGIAO.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_PHUONGTHUCGIAO.Size = new System.Drawing.Size(364, 44);
+            this.txt_PHUONGTHUCGIAO.TabIndex = 6;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 89);
+            this.label7.Location = new System.Drawing.Point(12, 95);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(71, 13);
             this.label7.TabIndex = 170;
@@ -367,18 +373,18 @@
             // 
             // txt_NGAYHIEULUC
             // 
-            this.txt_NGAYHIEULUC.Location = new System.Drawing.Point(100, 138);
+            this.txt_NGAYHIEULUC.Location = new System.Drawing.Point(100, 144);
             this.txt_NGAYHIEULUC.Mask = "00/00/0000";
             this.txt_NGAYHIEULUC.Name = "txt_NGAYHIEULUC";
             this.txt_NGAYHIEULUC.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.txt_NGAYHIEULUC.Size = new System.Drawing.Size(105, 20);
-            this.txt_NGAYHIEULUC.TabIndex = 7;
+            this.txt_NGAYHIEULUC.Size = new System.Drawing.Size(82, 20);
+            this.txt_NGAYHIEULUC.TabIndex = 8;
             this.txt_NGAYHIEULUC.ValidatingType = typeof(System.DateTime);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 141);
+            this.label5.Location = new System.Drawing.Point(12, 148);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(72, 13);
             this.label5.TabIndex = 165;
@@ -392,16 +398,18 @@
             this.txt_PHUONGTHUCTT.FormNametoShow = "";
             this.txt_PHUONGTHUCTT.Is_DateTime = false;
             this.txt_PHUONGTHUCTT.IsShowForm = false;
-            this.txt_PHUONGTHUCTT.Location = new System.Drawing.Point(100, 62);
+            this.txt_PHUONGTHUCTT.Location = new System.Drawing.Point(100, 40);
+            this.txt_PHUONGTHUCTT.Multiline = true;
             this.txt_PHUONGTHUCTT.Name = "txt_PHUONGTHUCTT";
             this.txt_PHUONGTHUCTT.Regular_Expression = null;
-            this.txt_PHUONGTHUCTT.Size = new System.Drawing.Size(390, 20);
+            this.txt_PHUONGTHUCTT.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_PHUONGTHUCTT.Size = new System.Drawing.Size(364, 44);
             this.txt_PHUONGTHUCTT.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Location = new System.Drawing.Point(12, 40);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 13);
             this.label3.TabIndex = 161;
@@ -410,7 +418,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(309, 16);
+            this.label1.Location = new System.Drawing.Point(306, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 158;
@@ -418,11 +426,11 @@
             // 
             // txt_NGAYBAOGIA
             // 
-            this.txt_NGAYBAOGIA.Location = new System.Drawing.Point(384, 14);
+            this.txt_NGAYBAOGIA.Location = new System.Drawing.Point(382, 13);
             this.txt_NGAYBAOGIA.Mask = "00/00/0000";
             this.txt_NGAYBAOGIA.Name = "txt_NGAYBAOGIA";
             this.txt_NGAYBAOGIA.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.txt_NGAYBAOGIA.Size = new System.Drawing.Size(105, 20);
+            this.txt_NGAYBAOGIA.Size = new System.Drawing.Size(82, 20);
             this.txt_NGAYBAOGIA.TabIndex = 1;
             this.txt_NGAYBAOGIA.ValidatingType = typeof(System.DateTime);
             // 
@@ -434,7 +442,7 @@
             this.txt_TENKHACH.FormNametoShow = "";
             this.txt_TENKHACH.Is_DateTime = false;
             this.txt_TENKHACH.IsShowForm = false;
-            this.txt_TENKHACH.Location = new System.Drawing.Point(212, 37);
+            this.txt_TENKHACH.Location = new System.Drawing.Point(640, 13);
             this.txt_TENKHACH.Name = "txt_TENKHACH";
             this.txt_TENKHACH.ReadOnly = true;
             this.txt_TENKHACH.Regular_Expression = null;
@@ -450,10 +458,10 @@
             this.txt_MAKHACH.FormNametoShow = "";
             this.txt_MAKHACH.Is_DateTime = false;
             this.txt_MAKHACH.IsShowForm = false;
-            this.txt_MAKHACH.Location = new System.Drawing.Point(100, 37);
+            this.txt_MAKHACH.Location = new System.Drawing.Point(554, 13);
             this.txt_MAKHACH.Name = "txt_MAKHACH";
             this.txt_MAKHACH.Regular_Expression = null;
-            this.txt_MAKHACH.Size = new System.Drawing.Size(105, 20);
+            this.txt_MAKHACH.Size = new System.Drawing.Size(82, 20);
             this.txt_MAKHACH.TabIndex = 2;
             this.txt_MAKHACH.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_MAKHACH_KeyDown);
             this.txt_MAKHACH.Validating += new System.ComponentModel.CancelEventHandler(this.txt_MAKHACH_Validating);
@@ -461,7 +469,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Location = new System.Drawing.Point(486, 17);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 13);
             this.label2.TabIndex = 126;
@@ -475,7 +483,7 @@
             this.pne_CHITIET.Location = new System.Drawing.Point(14, 240);
             this.pne_CHITIET.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.pne_CHITIET.Name = "pne_CHITIET";
-            this.pne_CHITIET.Size = new System.Drawing.Size(855, 209);
+            this.pne_CHITIET.Size = new System.Drawing.Size(904, 209);
             this.pne_CHITIET.TabIndex = 13;
             // 
             // btn_KHOIPHUC
@@ -559,13 +567,13 @@
             this.txt_SOBAOGIA.Location = new System.Drawing.Point(100, 13);
             this.txt_SOBAOGIA.Name = "txt_SOBAOGIA";
             this.txt_SOBAOGIA.Regular_Expression = null;
-            this.txt_SOBAOGIA.Size = new System.Drawing.Size(105, 20);
+            this.txt_SOBAOGIA.Size = new System.Drawing.Size(82, 20);
             this.txt_SOBAOGIA.TabIndex = 0;
             // 
             // label22
             // 
             this.label22.AutoSize = true;
-            this.label22.Location = new System.Drawing.Point(12, 16);
+            this.label22.Location = new System.Drawing.Point(12, 17);
             this.label22.Name = "label22";
             this.label22.Size = new System.Drawing.Size(58, 13);
             this.label22.TabIndex = 119;
@@ -575,7 +583,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1395, 548);
+            this.ClientSize = new System.Drawing.Size(1432, 548);
             this.Controls.Add(this.uiPanel1);
             this.Controls.Add(this.uiPanel0);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
