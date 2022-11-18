cf464edc83fa42e65c431b6c8baeddeb7497b6e5
DESKTOP-QD7JEVF\ADMIN <thepoorman1998@gmail.com>
2022-11-18 10:50:06 +0700

    cường - báo cáo nhập xuất tồn kho màng

--- GD.BBPH.APP/HETHONG/FRM_MAIN.cs
+++ GD.BBPH.APP/HETHONG/FRM_MAIN.cs
@@ -1189,7 +1189,13 @@ namespace GD.BBPH.APP.HETHONG
                         if (frm.Name == "FRM_BAOCAONHAPXUATTONKHONGUYENLIEU") { frm.Activate(); return; }
                     }
                     ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BAOCAONHAPXUATTONKHONGUYENLIEU()), mmru.Text);
-
+                    break;
+                case "FRM_BAOCAONHAPXUATTONKHOMANG":
+                    foreach (GD.BBPH.CONTROL.FORM frm in this.MdiChildren)
+                    {
+                        if (frm.Name == "FRM_BAOCAONHAPXUATTONKHOMANG") { frm.Activate(); return; }
+                    }
+                    ShowAsMDIChild((new GD.BBPH.APP.BAOCAO.FRM_BAOCAONHAPXUATTONKHOMANG()), mmru.Text);
                     break;
                 case "FRM_BAOCAOXUATNGUYENLIEU":
                     foreach (GD.BBPH.CONTROL.FORM frm in this.MdiChildren)
