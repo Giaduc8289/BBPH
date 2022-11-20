e9746d174d902ae856bbb9698f21241380d637cf
hung <hungtv2012@gmail.com>
2022-11-20 10:45:16 +0700

    Hiệu chỉnh hàm tính tốc độ máy thêm biến Mã màng

--- Tailieu/CSDL/Procedure/Tinhtocdomay.sql
+++ Tailieu/CSDL/Procedure/Tinhtocdomay.sql
@@ -13,9 +13,8 @@ BEGIN
 			@v_Dai				Decimal(20,3),
 			@v_Somauma				Nvarchar(50),
 			@v_Sohinhma				Nvarchar(50),
-			@v_Maloaimang			Nvarchar(MAX),
-			@v_Maloaimangghep1		Nvarchar(50),
-			@v_Maloaimangghep2		Nvarchar(MAX),
+			@v_Maloaimang			Nvarchar(50),-----Mã loại màng mang ghép
+			@v_Maloaimangs			Nvarchar(MAX),-----Tất cả các mã loại màng còn lại của sp
 			@v_Maqcthanhpham		Nvarchar(50),
 			@v_Makhach				Nvarchar(50),
 			@v_Tocdo			Decimal(20,3)
@@ -42,33 +41,22 @@ BEGIN
 		From dmhang sp 			
 		Where sp.Masp=@Masanpham
 		
-		Set @v_Maloaimangghep1 = IsNull((Select Top 1 Maloaimang From dmmang Where Mamang=@Mamang),'')
+		Set @v_Maloaimang = IsNull((Select Top 1 Maloaimang From dmmang Where Mamang=@Mamang),'')
 
-		DECLARE @Loaimang2 TABLE (Maloaimang Nvarchar(50))
-		INSERT INTO @Loaimang2(Maloaimang)
-			Select Distinct Maloaimang
-			From mangcuahang 
-			Where Masp=@Masanpham And Mamang != @Mamang
-
-		Select @v_Maloaimangghep2
-			= STUFF((Select ',' + Maloaimang
-					From @Loaimang2 t1
-					For XML Path('')), 1, Len(','), '')
-	
 		DECLARE @Loaimang TABLE (Maloaimang Nvarchar(50))
 		INSERT INTO @Loaimang(Maloaimang)
 			Select Distinct Maloaimang
 			From mangcuahang 
-			Where Masp=@Masanpham 
+			Where Masp=@Masanpham And Mamang != @Mamang
 
-		Select @v_Maloaimang
+		Select @v_Maloaimangs
 			= STUFF((Select ',' + Maloaimang
 					From @Loaimang t1
 					For XML Path('')), 1, Len(','), '')
-
+	
 		Select @v_Tocdo=Min(Tocdo)
-		From congsuatmay cs, (Select Value As Maloaimang From dbo.SplitString(@v_Maloaimang,',')) a
-			, (Select Value As Maloaimang2 From dbo.SplitString(@v_Maloaimangghep2,',')) b
+		From congsuatmay cs
+			, (Select Value As Maloaimang2 From dbo.SplitString(@v_Maloaimangs,',')) a
 		Where (CHARINDEX(@Mamay+',',Mamays)>0 OR ISNULL(Mamays,'') = '')
 			--And (CHARINDEX(@Macongdoan+',',Macongdoans)>0 OR ISNULL(Macongdoans,'') = '')
 			And ((Dodaytu<=@v_Doday And Dodayden>=@v_Doday) Or (Dodaytu=0 And Dodayden=0))	
@@ -77,21 +65,18 @@ BEGIN
 			AND	(CHARINDEX(@v_Sohinhma+',',Sohinhmas)>0 OR ISNULL(Sohinhmas,'') = '')
 			AND	(CHARINDEX(@v_Somauma+',',Somaumas)>0 OR ISNULL(Somaumas,'') = '')
 			AND (CHARINDEX(@v_Maqcthanhpham+',',Maqcthanhphams)>0 OR ISNULL(Maqcthanhphams,'') = '')			
-			AND	(CHARINDEX(a.Maloaimang+',',cs.Maloaimangs1)>0 OR ISNULL(cs.Maloaimangs1,'') = '')
 			AND (CHARINDEX(@v_Makhach+',',Makhachs)>0 OR ISNULL(Makhachs,'') = '')			
 			AND (CHARINDEX(@Masanpham+',',Mahangs)>0 OR ISNULL(Mahangs,'') = '')	
+			--AND	(CHARINDEX(a.Maloaimang+',',cs.Maloaimangs1)>0 OR ISNULL(cs.Maloaimangs1,'') = '')
+			--AND	(CHARINDEX(@v_Maloaimangghep1+',',cs.Maloaimangs1)>0 OR ISNULL(cs.Maloaimangs1,'') = '')
 			AND (
-					(
-						(CHARINDEX(@v_Maloaimangghep1+',',cs.Maloaimangs1)>0 OR ISNULL(cs.Maloaimangs1,'') = '') 
-						And 
-						(CHARINDEX(b.Maloaimang2+',',cs.Maloaimangs2)>0	OR IsNull(cs.Maloaimangs2,'') = '')
-					)
+					( ISNULL(cs.Maloaimangs1,'') = '' And IsNull(cs.Maloaimangs2,'') = '' )--Không kiểm tra điều kiện loại màng (2 trường loại màng đều null)
 					OR
-					(
-						(CHARINDEX(@v_Maloaimangghep1+',',cs.Maloaimangs2)>0 OR ISNULL(cs.Maloaimangs2,'') = '') 
-						And 
-						(CHARINDEX(b.Maloaimang2+',',cs.Maloaimangs1)>0	OR IsNull(cs.Maloaimangs1,'') = '')
-					)
+					( CHARINDEX(@v_Maloaimang+',',cs.Maloaimangs1)>0 And IsNull(cs.Maloaimangs2,'') = '' )--Chỉ kiểm tra loại màng mang ghép (Maloaimangs2 null) 
+					OR
+					( CHARINDEX(@v_Maloaimang+',',cs.Maloaimangs1)>0 And CHARINDEX(a.Maloaimang2+',',cs.Maloaimangs2)>0 )--Kiểm tra điều kiện 2 màng ghép với nhau
+					OR
+					( CHARINDEX(@v_Maloaimang+',',cs.Maloaimangs2)>0 And CHARINDEX(a.Maloaimang2+',',cs.Maloaimangs1)>0 )--Kiểm tra điều kiện 2 màng ghép với nhau
 				)
 
 	End
@@ -101,7 +86,11 @@ END
 	
 Go
 
-SELECT dbo.fTinhtocdomay('G2', 'SP 1566', 'PE 85x800') --('T1', 'CPP50x1120')
+SELECT dbo.fTinhtocdomay('T1', 'CPP50x1120', '')
+SELECT dbo.fTinhtocdomay('L1', 'SP 1566', 'MPET 12x800')
+SELECT dbo.fTinhtocdomay('L1', 'SP 1566', 'PE 85x800')
+SELECT dbo.fTinhtocdomay('G2', 'SP 1566', 'MPET 12x800')
+SELECT dbo.fTinhtocdomay('G2', 'SP 1566', 'PE 85x800') 
 
 GO	
 
